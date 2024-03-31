3b77605dd264d35f049670fa4f0dd54ac23e79ea
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-03-31 14:29:52 +0530

    add image button transfered edit_document_page from document_page

--- lib/pages/document_page.dart
+++ lib/pages/document_page.dart
@@ -1,9 +1,5 @@
 import 'dart:convert';
-// import 'dart:convert';
 import 'package:dio/dio.dart';
-import 'dart:io';
-// import 'package:http/http.dart' as http;
-import 'package:docscanner/components/my_button.dart';
 import 'package:docscanner/components/my_custom_home_page_transition.dart';
 import 'package:docscanner/components/my_pdf_view.dart';
 import 'package:docscanner/components/my_snack_bar.dart';
@@ -11,8 +7,6 @@ import 'package:docscanner/pages/edit_document_page.dart';
 import 'package:docscanner/services/database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
 
 class DocumentPage extends StatefulWidget {
   final String documentName;
@@ -33,8 +27,6 @@ double screenHeight = 0.0;
 double screenWidth = 0.0;
 
 class _DocumentPageState extends State<DocumentPage> {
-  File? imageFile;
-  final picker = ImagePicker();
   List<String> listDocumentValues = [];
   void back() {
     Navigator.pop(context);
@@ -116,181 +108,6 @@ class _DocumentPageState extends State<DocumentPage> {
     print(listDocumentValues);
   }
 
-  _cropImage(File pickedImage) async {
-    final croppedFile = await ImageCropper().cropImage(
-      sourcePath: pickedImage.path,
-      aspectRatioPresets: Platform.isAndroid
-          ? [
-              CropAspectRatioPreset.square,
-              CropAspectRatioPreset.ratio3x2,
-              CropAspectRatioPreset.original,
-              CropAspectRatioPreset.ratio16x9,
-              CropAspectRatioPreset.ratio4x3,
-            ]
-          : [
-              CropAspectRatioPreset.square,
-              CropAspectRatioPreset.ratio3x2,
-              CropAspectRatioPreset.original,
-              CropAspectRatioPreset.ratio16x9,
-              CropAspectRatioPreset.ratio4x3,
-              CropAspectRatioPreset.ratio5x3,
-              CropAspectRatioPreset.ratio5x4,
-              CropAspectRatioPreset.ratio7x5,
-            ],
-      uiSettings: [
-        AndroidUiSettings(
-          toolbarColor: const Color(0xFFF4BBFF),
-          toolbarTitle: "Crop the selected image",
-          toolbarWidgetColor: Colors.black,
-          initAspectRatio: CropAspectRatioPreset.original,
-          lockAspectRatio: false,
-          activeControlsWidgetColor: const Color(0xFFF4BBFF),
-        ),
-        IOSUiSettings(
-          title: "Crop the selected image",
-          aspectRatioLockEnabled: false,
-        )
-      ],
-    );
-
-    //image cropped
-    if (croppedFile != null) {
-      imageCache.clear();
-      setState(() {
-        imageFile = File(croppedFile.path);
-      });
-
-      try {
-        showDialog(
-          context: context,
-          builder: (context) => const Center(
-            child: CircularProgressIndicator(
-              color: Color(0xFFF4BBFF),
-            ),
-          ),
-        );
-
-        final imageUploadSuccess = await addNewImage(
-          context,
-          widget.documentName,
-          imageFile!,
-          widget.uId,
-        );
-        if (imageUploadSuccess) {
-          // Navigator.pop(context);
-          mySnackBar(
-            context,
-            'Document created successfully',
-            Colors.green,
-          );
-          getDocumentValue(widget.uId);
-        } else {
-          // Navigator.pop(context);
-          mySnackBar(
-            context,
-            'Failed to create document',
-            Colors.red,
-          );
-        }
-      } catch (e) {
-        mySnackBar(context, e.toString(), Colors.red);
-      }
-    }
-  }
-
-  _imagePickFromGallery() async {
-    await picker
-        .pickImage(source: ImageSource.gallery, imageQuality: 100)
-        .then((pickedImage) {
-      if (pickedImage != null) {
-        _cropImage(File(pickedImage.path));
-      }
-    });
-  }
-
-  _imagePickFromCamera() async {
-    await picker
-        .pickImage(source: ImageSource.camera, imageQuality: 100)
-        .then((pickedImage) {
-      if (pickedImage != null) {
-        _cropImage(File(pickedImage.path));
-      }
-    });
-  }
-
-  Future _displayAddNewImageBottomSheet(BuildContext context) {
-    screenWidth = MediaQuery.of(context).size.width;
-    screenHeight = MediaQuery.of(context).size.height;
-    return showModalBottomSheet(
-      barrierColor: Colors.black.withOpacity(0.5),
-      backgroundColor: Colors.black,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(10),
-        ),
-      ),
-      context: context,
-      builder: (context) => SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Row(
-                    children: [
-                      Text(
-                        "Select Image From",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.03,
-                  ),
-                  Column(
-                    children: [
-                      MyButton(
-                        height: 55,
-                        width: double.infinity,
-                        buttonText: "Gallery",
-                        onTap: () {
-                          Navigator.pop(context);
-                          _imagePickFromGallery();
-                        },
-                        buttonColor: const Color(0xFFF4BBFF),
-                      ),
-                      SizedBox(
-                        height: screenHeight * 0.03,
-                      ),
-                      MyButton(
-                        height: 55,
-                        width: double.infinity,
-                        buttonText: "Camera",
-                        onTap: () {
-                          _imagePickFromCamera();
-                        },
-                        buttonColor: const Color(0xFFF4BBFF),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.02,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
@@ -298,24 +115,6 @@ class _DocumentPageState extends State<DocumentPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: Colors.black, width: 2.5),
-            ),
-            child: MyButton(
-              height: 55,
-              width: 130,
-              buttonText: "Add Image",
-              onTap: () {
-                _displayAddNewImageBottomSheet(context);
-              },
-              buttonColor: const Color(0xFFF4BBFF),
-            ),
-          ),
-        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
--- lib/pages/edit_document_page.dart
+++ lib/pages/edit_document_page.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
+import 'package:docscanner/components/my_button.dart';
 import 'package:docscanner/components/my_custom_home_page_transition.dart';
 import 'package:docscanner/components/my_pdf_view.dart';
 import 'package:docscanner/components/my_snack_bar.dart';
+import 'package:docscanner/pages/document_page.dart';
 import 'package:docscanner/pages/home_page.dart';
 import 'package:docscanner/services/database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 
 class EditDocumentPage extends StatefulWidget {
@@ -27,7 +33,8 @@ double screenWidth = 0.0;
 
 class _EditDocumentPageState extends State<EditDocumentPage> {
   late List<String> _editedImages;
-
+  File? imageFile;
+  final picker = ImagePicker();
   List<dynamic> updatedListDocumentValues = [];
   void back() {
     Navigator.pop(context);
@@ -70,6 +77,189 @@ class _EditDocumentPageState extends State<EditDocumentPage> {
     _editedImages = List.from(widget.currentListDocumentValues);
   }
 
+  _cropImage(File pickedImage) async {
+    final croppedFile = await ImageCropper().cropImage(
+      sourcePath: pickedImage.path,
+      aspectRatioPresets: Platform.isAndroid
+          ? [
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio3x2,
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.ratio16x9,
+              CropAspectRatioPreset.ratio4x3,
+            ]
+          : [
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio3x2,
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.ratio16x9,
+              CropAspectRatioPreset.ratio4x3,
+              CropAspectRatioPreset.ratio5x3,
+              CropAspectRatioPreset.ratio5x4,
+              CropAspectRatioPreset.ratio7x5,
+            ],
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarColor: const Color(0xFFF4BBFF),
+          toolbarTitle: "Crop the selected image",
+          toolbarWidgetColor: Colors.black,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false,
+          activeControlsWidgetColor: const Color(0xFFF4BBFF),
+        ),
+        IOSUiSettings(
+          title: "Crop the selected image",
+          aspectRatioLockEnabled: false,
+        )
+      ],
+    );
+
+    //image cropped
+    if (croppedFile != null) {
+      imageCache.clear();
+      setState(() {
+        imageFile = File(croppedFile.path);
+      });
+
+      try {
+        showDialog(
+          context: context,
+          builder: (context) => const Center(
+            child: CircularProgressIndicator(
+              color: Color(0xFFF4BBFF),
+            ),
+          ),
+        );
+
+        final imageUploadSuccess = await addNewImage(
+          context,
+          widget.documentName,
+          imageFile!,
+          widget.uId,
+        );
+        if (imageUploadSuccess) {
+          // Navigator.pop(context);
+          mySnackBar(
+            context,
+            'image added successfully',
+            Colors.green,
+          );
+          // getDocumentValue(widget.uId);
+          Navigator.pushReplacement(
+            context,
+            MyCustomHomePageRoute(
+              DocumentPage(
+                documentName: widget.documentName,
+                uId: widget.uId,
+              ),
+            ),
+          );
+        } else {
+          mySnackBar(
+            context,
+            'Failed to create document',
+            Colors.red,
+          );
+        }
+      } catch (e) {
+        mySnackBar(context, e.toString(), Colors.red);
+      }
+    }
+  }
+
+  _imagePickFromGallery() async {
+    await picker
+        .pickImage(source: ImageSource.gallery, imageQuality: 100)
+        .then((pickedImage) {
+      if (pickedImage != null) {
+        _cropImage(File(pickedImage.path));
+      }
+    });
+  }
+
+  _imagePickFromCamera() async {
+    await picker
+        .pickImage(source: ImageSource.camera, imageQuality: 100)
+        .then((pickedImage) {
+      if (pickedImage != null) {
+        _cropImage(File(pickedImage.path));
+      }
+    });
+  }
+
+  Future _displayAddNewImageBottomSheet(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    return showModalBottomSheet(
+      barrierColor: Colors.black.withOpacity(0.5),
+      backgroundColor: Colors.black,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(10),
+        ),
+      ),
+      context: context,
+      builder: (context) => SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Row(
+                    children: [
+                      Text(
+                        "Select Image From",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.03,
+                  ),
+                  Column(
+                    children: [
+                      MyButton(
+                        height: 55,
+                        width: double.infinity,
+                        buttonText: "Gallery",
+                        onTap: () {
+                          Navigator.pop(context);
+                          _imagePickFromGallery();
+                        },
+                        buttonColor: const Color(0xFFF4BBFF),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      MyButton(
+                        height: 55,
+                        width: double.infinity,
+                        buttonText: "Camera",
+                        onTap: () {
+                          _imagePickFromCamera();
+                        },
+                        buttonColor: const Color(0xFFF4BBFF),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
@@ -77,6 +267,24 @@ class _EditDocumentPageState extends State<EditDocumentPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.black, width: 2.5),
+            ),
+            child: MyButton(
+              height: 55,
+              width: 130,
+              buttonText: "Add Image",
+              onTap: () {
+                _displayAddNewImageBottomSheet(context);
+              },
+              buttonColor: const Color(0xFFF4BBFF),
+            ),
+          ),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
